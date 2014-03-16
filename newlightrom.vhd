a16d9a88049129bf1a06f2bf7c3172df3d805ac5
imtibbet <tibbian@gmail.com>
2014-03-16 14:24:44 -0400

    Update newlightrom.vhd
    implemented project

--- newlightrom.vhd
+++ newlightrom.vhd
@@ -18,22 +18,39 @@ begin
 
 --uncomment below to see the given testing program
 data(9 downto 0) <= 
-      "0000101000" when addr(3 downto 0) = "0000" else -- move 0s to LR  00000000
-      "0010101010" when addr(3 downto 0) = "0001" else -- bit invert LR  11111111
-      "0010111010" when addr(3 downto 0) = "0010" else -- bit invert LR  00000000
-      "0111101100" when addr(3 downto 0) = "0011" else -- bit invert LR  11111111
-      "0111001100" when addr(3 downto 0) = "0100" else -- shift LR right 01111111
-      "0100110001" when addr(3 downto 0) = "0101" else -- shift LR right 00111111
-      "1100000011" when addr(3 downto 0) = "0110" else -- rotate LR left 01111110
-      "1000000000"; --when addr(3 downto 0) = "0111" else -- rotate LR left 11111100
---      "111" when addr(3 downto 0) = "1000" else -- rotate LR left 11111001
---      "111" when addr(3 downto 0) = "1001" else -- rotate LR left 11110011
---      "010" when addr(3 downto 0) = "1010" else -- shift LR left  11100110
---      "010" when addr(3 downto 0) = "1011" else -- shift LR left  11001100
---      "011" when addr(3 downto 0) = "1100" else -- add 1 to LR    11001101
---      "100" when addr(3 downto 0) = "1101" else -- sub 1 from LR  11001100
---      "101" when addr(3 downto 0) = "1110" else -- bit invert LR  00110011
---      "011";                        -- add 1 to LR    00110100
---
+	"0001100000" when addr = "0000" else -- move 0s to LR   00000000/00000000
+    "0011100001" when addr = "0001" else -- move 0001 to ACC hi4   00000000/00010000
+    "0001000000" when addr = "0010" else -- move ACC to LR 00010000/00010000
+    "0100011100" when addr = "0011" else -- add -1 to LR   11111010/00010000
+    "1110000110" when addr = "0100" else -- branch to 0110 if LR is zero
+    "1000000011" when addr = "0101" else -- branch above umcond to 0011 
+    "0100011000" when addr = "0110" else -- add -1 to ACC 
+	 "0001110000" when addr = "0111" else -- set LR to 1s    11111111
+    "0110011100" when addr = "1000" else -- xor LR w/ all 1s  00000000
+    "0100011000" when addr = "1001" else -- add -1 to ACC 
+    "1100000000" when addr = "1010" else -- cond branch to 0000 if ACC is zero
+    "1000001000" when addr = "1011" else -- uncond branch to 1000
+    "0000000000" when addr = "1100" else -- garbage
+    "0000000000" when addr = "1101" else -- garbage
+    "0000000000" when addr = "1110" else -- garbage
+    "0000000000";                        -- garbage
+
+--	"0001100000" when addr = "0000" else -- move 0s to LR   00000000/00000000
+--    "0001110000" when addr = "0001" else -- move 1s to LR   11111111/00000000
+--    "0001101010" when addr = "0010" else -- move 1010 to LR 11111010/00000000
+--    "0010101000" when addr = "0011" else -- move 8 to ACC   11111010/00001000
+--    "0101001100" when addr = "0100" else -- shift LR left   
+--    "0100011000" when addr = "0101" else -- add -1 to ACC  
+--    "1100001000" when addr = "0110" else -- branch if  zero  
+--    "1000000100" when addr = "0111" else -- branch  
+--    "0001110000" when addr = "1000" else -- set LR to 1s    11111111/00000000
+--    "1000000000" when addr = "1001" else -- branch to zero  11111111/00000000
+--    "0101010101" when addr = "1010" else -- garbage
+--    "1010101010" when addr = "1011" else -- garbage
+--    "1100110011" when addr = "1100" else -- garbage
+--    "0011001100" when addr = "1101" else -- garbage
+--    "0000000000" when addr = "1110" else -- garbage
+--    "1111111111";                        -- garbage
+
 
 end rtl;
