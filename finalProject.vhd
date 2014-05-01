d64f6b5b8a1bd8d97a6137037764e6759bed0a05
imtibbet <tibbian@gmail.com>
2014-05-01 15:42:16 -0400

    have a working map of blocks!
    map is sweet, still need to implement reset

--- finalProject.vhd
+++ finalProject.vhd
@@ -48,14 +48,17 @@ end component;
 	signal counter : unsigned(1 downto 0);
 	signal slowclock : std_logic;
 
+	constant blockSize : integer := 30;
+	constant zeros : std_logic_vector(11 downto 0) := (others=>'0');
+	constant blockWidth : integer := 600/blockSize;
+	constant blockHeight : integer := 450/blockSize;
 	--A 2-d array declaration, from http://vhdlguru.blogspot.com/2010/02/arrays-and-records-in-vhdl.html
-	type rowBlocks is array (0 to 3) of std_logic_vector(11 downto 0);
-	type columnBlocks is array (0 to 2) of rowBlocks; 
-	signal blockGrid : columnBlocks;  --blockGrid is a row*column two dimensional array.
-	--initialization to zeros.
-	blockGrid <= ((others=>(others=>'0')),(others=>(others=>'0')),(others=>(others=>'0')));
-	--element access is given by blockGrid(0)(0), etc
-	--entire rows may be assigned by blockGrid(0) <= "0001000" for example 
+	type rowBlocks is array (0 to blockHeight) of std_logic_vector(11 downto 0); -- 12 bit vector in each cell, 4 bits per color
+	type columnBlocks is array (0 to blockWidth) of rowBlocks; 
+	signal blockGrid : columnBlocks;-- := --blockGrid is a row*column two dimensional array.
+--							 ((others=>(others=>'0')),(others=>(others=>'0')),(others=>(others=>'0')),(others=>(others=>'0')),
+--							  (others=>(others=>'0')),(others=>(others=>'0')),(others=>(others=>'0')),(others=>(others=>'0')),
+--							  (others=>(others=>'0')),(others=>(others=>'0')),(others=>(others=>'0')));
 begin
 	VGA: vgaDriver
 		port map(clk => slowclock, reset => resetT, 
@@ -64,32 +67,58 @@ begin
 
 	column <= hcounterT - 138;
 	row <= vcounterT - 35;
-			process (column, row)
-			begin
-				if column >= 0 and column < 640 then
-					if row >= 0 and row < 480 then
-					--useable area on screen, all pixels will be turned on from here
-					
-						if column = 0 or column = 639 or row = 0 or row = 479 then
-							blueOut <= "1001";
-						else
-							blueOut <= "0000";
-						end if;
-					
-					--end of useable area
-					else
-						redOut <= "0000";
-						greenOut <= "0000";
-						blueOut <= "0000";
-					end if;
-				else
-					redOut <= "0000";
-					greenOut <= "0000";
-					blueOut <= "0000";
+	
+	-- needs to change to clear dynamically based on array lengths, will be loops
+--	process (resetT)
+--	begin
+--		if resetT = '0' then
+--			--initialization to zeros.
+--			blockGrid <= ((others=>(others=>'0')),(others=>(others=>'0')),(others=>(others=>'0')),(others=>(others=>'0')),
+--							  (others=>(others=>'0')),(others=>(others=>'0')),(others=>(others=>'0')),(others=>(others=>'0')),
+--							  (others=>(others=>'0')),(others=>(others=>'0')),(others=>(others=>'0')));
+--			--element access is given by blockGrid(0)(0), etc
+--			--entire rows may be assigned by blockGrid(0) <= "0001000" for example 
+--		end if;
+--	end process;
+
+	process (column, row)
+	begin
+		
+		--useable area on screen, all pixels will be turned on from here
+		if column >= 0 and column < 640 then
+			if row >= 0 and row < 480 then
+				--initially off
+				blueOut <= "0000";	
+				
+				-- on for border
+				if column = 0 or column = 639 or row = 0 or row = 479 then
+					blueOut <= "1001";
 				end if;
-			end process;
-		
-		
+				
+				-- turn valid blocks on
+				for i in 0 to blockWidth loop
+					for j in 0 to blockHeight loop
+						if column > 1+(i*blockSize) and column < ((i+1)*blockSize) and row > 1+(j*blockSize) and row < ((j+1)*blockSize) then
+							blueOut <= "1001"; --<= blockGrid(i)(j)(3 downto 0)
+							--greenOut <= blockGrid(i)(j)(7 downto 4)
+							--redOut <= blockGrid(i)(j)(11 downto 8)
+						end if;
+					end loop;
+				end loop;
+				
+			--not useable area
+			else
+				redOut <= "0000";
+				greenOut <= "0000";
+				blueOut <= "0000";
+			end if;
+		else
+			redOut <= "0000";
+			greenOut <= "0000";
+			blueOut <= "0000";
+		end if;
+	end process;
+	
 --process to slow 50MHz clock to 25MHz
 	process (clkT)
 	begin
